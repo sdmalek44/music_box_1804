5da17d965c8b7863dff5175d8ad000942b065cc9
Stephen Malek <malek.stephen44@gmail.com>
2018-07-06 09:49:56 -0600

    add artist model and migration

---
+++ spec/features/user_sees_all_songs_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'user index' do
+  it 'user can see all songs' do
+    song1 = Song.create(title: "Don't Stop Believin", length: 303, play_count: 123456)
+    song2 = Song.create(title: "Threshold", length: 253, play_count: 9876234)
+
+    visit '/songs'
+
+    expect(page).to have_content("All Songs")
+    expect(page).to have_content(song1.title)
+    expect(page).to have_content("Plays: #{song1.play_count}")
+    expect(page).to have_content(song2.title)
+    expect(page).to have_content("Plays: #{song2.play_count}")
+  end
+end
