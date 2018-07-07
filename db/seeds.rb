6bf0b109ec050b630856a6ea4bc675874e9ab6e8
Stephen Malek <malek.stephen44@gmail.com>
2018-07-06 21:03:22 -0600

    style song page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+artist = Artist.create(name: 'slayer')
+artist.songs.create(title: "catalyst", length: 208, play_count: 78900)
+artist.songs.create(title: "catalyst", length: 208, play_count: 78900)
+artist.songs.create(title: "catalyst", length: 208, play_count: 78900)
+artist.songs.create(title: "catalyst", length: 208, play_count: 78900)
+artist.songs.create(title: "catalyst", length: 208, play_count: 78900)
+artist.songs.create(title: "catalyst", length: 208, play_count: 78900)
+artist.songs.create(title: "catalyst", length: 208, play_count: 78900)
+artist.songs.create(title: "catalyst", length: 208, play_count: 78900)
+artist.songs.create(title: "catalyst", length: 208, play_count: 78900)
+artist.songs.create(title: "catalyst", length: 208, play_count: 78900)
+artist.songs.create(title: "catalyst", length: 208, play_count: 78900)
